e2bd003b54e64796726fbe241f7022cc1f0a9ecf
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-10 13:09:36 -0400

    Add pet dictionaries and print them

---
+++ pets_6-8.py
@@ -0,0 +1,8 @@
+Dog = {'Corgi': 'Princess'}
+Cat = {'Chihuahua': 'Precious'}
+Hamster = {'Syrian': 'James'}
+Pig = {'Wild Boar': 'Chris'}
+
+pets = [Dog, Cat, Hamster, Pig]
+for pet in pets:
+    print(pet)
