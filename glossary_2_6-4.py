e00812e792d0a4334626de96bf43f3aecdd07a20
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-10 12:20:12 -0400

    Add glossary of Python terms with definitions

---
+++ glossary_2_6-4.py
@@ -0,0 +1,9 @@
+python_words = {'print': 'Output value of specific thing',
+                'variable': 'Single data value of something',
+                'loop': 'Go through every value in info set',
+                'booleans': 'True or False',
+                'floats': 'Decimals',
+                'nesting': 'putting one thing inside another'
+                }
+for word in python_words:
+    print(word.title())
