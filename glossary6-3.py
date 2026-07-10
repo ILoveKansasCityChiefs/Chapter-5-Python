9744b55d098ab70ce4bdb9dead13a0709e1dc572
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-10 11:52:43 -0400

    Add Python words dictionary and print definitions
    Initialize a dictionary of Python terminology and print definitions.

---
+++ glossary6-3.py
@@ -0,0 +1,11 @@
+python_words = {'print': 'Output value of specific thing',
+                'variable': '\nSingle data value of something',
+                'loop': '\nGo through every value in info set',
+                'booleans': '\nTrue or False',
+                'floats': '\nDecimals'
+                }
+print(python_words['print'])
+print(python_words['variable'])
+print(python_words['loop'])
+print(python_words['booleans'])
+print(python_words['floats'])
