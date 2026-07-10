0ea462de88e25ce49ca1945271cf36496d3cf1c9
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-10 12:59:23 -0400

    Add person dictionaries and print them

---
+++ Person_2_6-7.py
@@ -0,0 +1,6 @@
+Me = {'first_name': 'Christopher', 'last_name': 'White', 'age': 14, 'city': 'NYC'}
+Albert_E = {'first_name': 'Albert', 'last_name': 'Einstein', 'age': 76, 'city': 'Princeton'}
+Nikola_T = {'first_name': 'Nikola', 'last_name': 'Tesla', 'age': 86, 'city': 'NYC'}
+people = [Me, Albert_E, Nikola_T]
+for person in people:
+    print(person)
