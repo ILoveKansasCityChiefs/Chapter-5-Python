767fb112a47f347a78c7448d5b0645c0ee946c62
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-10 11:46:03 -0400

    Add NFL teams and player mappings

---
+++ NFLteams.py
@@ -0,0 +1,11 @@
+nfl = {'Tom Brady': 'Bucaneers',
+        'Pat Mahomes': 'Chiefs',
+        'Sam Darnold': 'Seahawks',
+        'Joe Burrow': 'Bengals',
+         'Jordan Love': 'Packers'
+        }
+print(nfl['Tom Brady'])
+print(nfl['Pat Mahomes'])
+print(nfl['Sam Darnold'])
+print(nfl['Joe Burrow'])
+print(nfl['Jordan Love'])
